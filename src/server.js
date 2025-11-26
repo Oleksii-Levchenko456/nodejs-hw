c0e77cc37aeac53d454042c91f100fedad9880b1
Oleksii Levchenko <neonator3000@gmail.com>
2025-11-26 13:33:19 +0200

    new commit

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ import pino from 'pino-http';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(
   pino({
     level: 'info',
@@ -27,29 +27,30 @@ app.use(
 
 
 app.get('/notes', (req, res) => {
-  res.status(200).json({ message: "Retrieved all notes" })
-})
+  res.status(200).json({ message: "Retrieved all notes" });
+});
 
-app.get('/notes/:notesId', (req, res) => {
-  const {notesId} = req.params
-res.status(200).json({ message: `Retrieved note with ID: ${notesId}` });})
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
 
-app.get('/test-error', ()=>{
-    throw new Error('Simulated server error');
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
 
-})
+});
 
-app.use((req, res)=>{
-  res.status(404).json({message: "Route not found"})
-})
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   console.error('Error:', err.message);
   res.status(500).json({
     message: 'Internal Server Error',
     error: err.message,
-  })
-})
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
