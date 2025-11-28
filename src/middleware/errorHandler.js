37110c424added7266fab2436413eb7266e00819
Oleksii Levchenko <neonator3000@gmail.com>
2025-11-28 16:44:43 +0200

    added next in meddleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,6 @@
 import { HttpError } from 'http-errors';
 
-export const errorHandler = (err, req, res) => {
+export const errorHandler = (err, req, res, next) => {
   console.error('Error Middleware:', err)
 
   if (err instanceof HttpError) {
