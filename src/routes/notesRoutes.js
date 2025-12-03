97fc7b80a0b4225259c2e5df5b808762acf216a2
Oleksii Levchenko <neonator3000@gmail.com>
2025-12-04 01:25:29 +0200

    fixed createNoteSchema

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -5,7 +5,7 @@ import {
   getNoteById, updateNote
 } from '../controllers/notesController.js'
 import { celebrate } from 'celebrate'
-import { createNoteSchema, getAllNotesSchema, noteIdSchema } from '../validations/notesValidation.js'
+import { createNoteSchema, getAllNotesSchema, noteIdSchema, updateNoteSchema } from '../validations/notesValidation.js'
 
 
 const router = Router()
@@ -14,7 +14,7 @@ router.get('/notes', celebrate(getAllNotesSchema), getAllNotes)
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById)
 router.post('/notes', celebrate(createNoteSchema), createNote)
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote)
-router.patch('/notes/:noteId', updateNote)
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote)
 
 
 export default router
